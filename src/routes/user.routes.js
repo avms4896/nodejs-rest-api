6c4ef55610f915ae19bae97319b8fe6ad7019283
Avneesh Mishra <avms4896@gmail.com>
2025-12-26 11:17:45 +0530

    Update user.routes.js

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,3 +1,29 @@
+/**
+ * @swagger
+ * /api/auth/login:
+ *   post:
+ *     summary: Login user
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - email
+ *               - password
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Login successful
+ */
+
+
 const express = require("express");
 const router = express.Router();
 
