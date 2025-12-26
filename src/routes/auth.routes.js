6438f3eaaa39fdb0f1033f5498bf9e4d3cc7dafb
Avneesh Mishra <avms4896@gmail.com>
2025-12-26 11:05:13 +0530

    Update auth.routes.js
    Apply request validation to auth routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,27 @@
 const express = require("express");
 const router = express.Router();
-const {
-  register,
-  login
-} = require("../controllers/auth.controller");
 
-router.post("/register", register);
-router.post("/login", login);
+const { register, login } = require("../controllers/auth.controller");
+const validate = require("../middleware/validate.middleware");
+const {
+  registerValidation,
+  loginValidation
+} = require("../validators/auth.validator");
+
+// Register
+router.post(
+  "/register",
+  registerValidation,
+  validate,
+  register
+);
+
+// Login
+router.post(
+  "/login",
+  loginValidation,
+  validate,
+  login
+);
 
 module.exports = router;
